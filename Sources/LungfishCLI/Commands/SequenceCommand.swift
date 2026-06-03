9eb47b8a6e9721802e487634f93b56ade8f8d0b9
dho (raven) <dhoconno+raven@wisc.edu>
2026-06-03 09:21:20 -0500

    release: v0.5.0-alpha16

--- Sources/LungfishCLI/Commands/SequenceCommand.swift
+++ Sources/LungfishCLI/Commands/SequenceCommand.swift
@@ -255,7 +255,7 @@ struct SequenceCommand: AsyncParsableCommand {
 
 }
 
-private let cliVersion = "0.5.0-alpha15"
+private let cliVersion = "0.5.0-alpha16"
 
 private func parseReadingFrames(_ rawValue: String) throws -> [ReadingFrame] {
     let tokens = rawValue
