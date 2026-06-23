198e0dfbd53d4a7590f349a688fde8466e19d191
dho (raven) <dhoconno+raven@wisc.edu>
2026-06-22 23:21:24 -0700

    release: v0.5.0-alpha26

--- Sources/LungfishCLI/Commands/SequenceCommand.swift
+++ Sources/LungfishCLI/Commands/SequenceCommand.swift
@@ -255,7 +255,7 @@ struct SequenceCommand: AsyncParsableCommand {
 
 }
 
-private let cliVersion = "0.5.0-alpha25"
+private let cliVersion = "0.5.0-alpha26"
 
 private func parseReadingFrames(_ rawValue: String) throws -> [ReadingFrame] {
     let tokens = rawValue
--- Tests/LungfishCLITests/CLIRegressionTests.swift
+++ Tests/LungfishCLITests/CLIRegressionTests.swift
@@ -27,7 +27,7 @@ final class CLITopLevelRegressionTests: XCTestCase {
     }
 
     func testLungfishCLIVersion() {
-        XCTAssertEqual(LungfishCLI.configuration.version, "0.5.0-alpha25")
+        XCTAssertEqual(LungfishCLI.configuration.version, "0.5.0-alpha26")
     }
 
     func testLungfishCLIAbstractIsNonEmpty() {
