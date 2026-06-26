fb03f00eca94551283e7a8f46d0150b5c3ce399b
dho (raven) <dhoconno+raven@wisc.edu>
2026-06-26 16:48:41 -0700

    release: v0.5.0-alpha33

--- Sources/LungfishCLI/LungfishCLI.swift
+++ Sources/LungfishCLI/LungfishCLI.swift
@@ -28,7 +28,7 @@ struct LungfishCLI: AsyncParsableCommand {
 
             For more information, see: https://github.com/dhoconno/lungfish-genome-explorer
             """,
-        version: "0.5.0-alpha32",
+        version: "0.5.0-alpha33",
         subcommands: [
             VersionCommand.self,
             ConvertCommand.self,
--- Tests/LungfishCLITests/CLIRegressionTests.swift
+++ Tests/LungfishCLITests/CLIRegressionTests.swift
@@ -27,7 +27,7 @@ final class CLITopLevelRegressionTests: XCTestCase {
     }
 
     func testLungfishCLIVersion() {
-        XCTAssertEqual(LungfishCLI.configuration.version, "0.5.0-alpha32")
+        XCTAssertEqual(LungfishCLI.configuration.version, "0.5.0-alpha33")
     }
 
     func testLungfishCLIAbstractIsNonEmpty() {
