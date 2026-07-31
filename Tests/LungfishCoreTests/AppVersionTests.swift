b78c9f20603a4677d6f1d081121f023b2a2694cd
dho (raven) <dhoconno+raven@wisc.edu>
2026-07-30 21:15:03 -0500

    release: prepare 0.5.0-beta12

--- Tests/LungfishCoreTests/AppVersionTests.swift
+++ Tests/LungfishCoreTests/AppVersionTests.swift
@@ -2,8 +2,8 @@ import XCTest
 @testable import LungfishCore
 
 final class AppVersionTests: XCTestCase {
-    func testCanonicalVersionIsBeta10() {
-        XCTAssertEqual(LungfishAppVersion.short, "0.5.0-beta11")
-        XCTAssertEqual(LungfishAppVersion.cliToolVersion, "lungfish-cli 0.5.0-beta11")
+    func testCanonicalVersionIsBeta12() {
+        XCTAssertEqual(LungfishAppVersion.short, "0.5.0-beta12")
+        XCTAssertEqual(LungfishAppVersion.cliToolVersion, "lungfish-cli 0.5.0-beta12")
     }
 }
