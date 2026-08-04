4a675a989d3004bf551140fd0b41d2a832a8cad7
dho (raven) <dhoconno+raven@wisc.edu>
2026-08-04 11:14:45 -0500

    release: v0.5.0-beta20

--- Sources/LungfishCore/AppVersion.swift
+++ Sources/LungfishCore/AppVersion.swift
@@ -3,6 +3,6 @@
 // SPDX-License-Identifier: MIT
 
 public enum LungfishAppVersion {
-    public static let short = "0.5.0-beta19"
+    public static let short = "0.5.0-beta20"
     public static let cliToolVersion = "lungfish-cli \(short)"
 }
--- Tests/LungfishCoreTests/AppVersionTests.swift
+++ Tests/LungfishCoreTests/AppVersionTests.swift
@@ -3,7 +3,7 @@ import XCTest
 
 final class AppVersionTests: XCTestCase {
     func testCanonicalVersionIsBeta17() {
-        XCTAssertEqual(LungfishAppVersion.short, "0.5.0-beta19")
-        XCTAssertEqual(LungfishAppVersion.cliToolVersion, "lungfish-cli 0.5.0-beta19")
+        XCTAssertEqual(LungfishAppVersion.short, "0.5.0-beta20")
+        XCTAssertEqual(LungfishAppVersion.cliToolVersion, "lungfish-cli 0.5.0-beta20")
     }
 }
