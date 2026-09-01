8d114b42bd846439689b96dd9f09f0754321ca51
Dave O'Connor <dhoconno@wisc.edu>
2026-09-01 03:17:49 -0500

    release: v2026.9.2

--- Tests/LungfishCoreTests/AppVersionTests.swift
+++ Tests/LungfishCoreTests/AppVersionTests.swift
@@ -3,7 +3,7 @@ import XCTest
 
 final class AppVersionTests: XCTestCase {
     func testCanonicalReleaseVersion() {
-        XCTAssertEqual(LungfishAppVersion.short, "2026.9.1")
-        XCTAssertEqual(LungfishAppVersion.cliToolVersion, "lungfish-cli 2026.9.1")
+        XCTAssertEqual(LungfishAppVersion.short, "2026.9.2")
+        XCTAssertEqual(LungfishAppVersion.cliToolVersion, "lungfish-cli 2026.9.2")
     }
 }
