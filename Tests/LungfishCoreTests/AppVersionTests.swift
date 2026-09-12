d05eca19bbaadf05798d85603a7eea63b119df9e
dho (raven) <dhoconno+raven@wisc.edu>
2026-09-11 20:22:43 -0500

    Prepare Preview 2026.9.19 release identity and notes

--- Tests/LungfishCoreTests/AppVersionTests.swift
+++ Tests/LungfishCoreTests/AppVersionTests.swift
@@ -3,7 +3,7 @@ import XCTest
 
 final class AppVersionTests: XCTestCase {
     func testCanonicalReleaseVersion() {
-        XCTAssertEqual(LungfishAppVersion.short, "2026.9.18")
-        XCTAssertEqual(LungfishAppVersion.cliToolVersion, "lungfish-cli 2026.9.18")
+        XCTAssertEqual(LungfishAppVersion.short, "2026.9.19")
+        XCTAssertEqual(LungfishAppVersion.cliToolVersion, "lungfish-cli 2026.9.19")
     }
 }
