3fc2179b8361069d103c40d85aa944c2ba44c7fd
dho (raven) <dhoconno+raven@wisc.edu>
2026-09-13 10:15:15 -0500

    release: v2026.9.21 Preview

--- Sources/LungfishCore/AppVersion.swift
+++ Sources/LungfishCore/AppVersion.swift
@@ -3,6 +3,6 @@
 // SPDX-License-Identifier: MIT
 
 public enum LungfishAppVersion {
-    public static let short = "2026.9.20"
+    public static let short = "2026.9.21"
     public static let cliToolVersion = "lungfish-cli \(short)"
 }
